213b22181f52d88f14c5b7a62b0f82a1aabe6b33
Ray-Escobar <pietrovigilanza7@gmail.com>
2020-01-04 11:19:51 +0100

    started to implement some JS functionality. Basic clicking on blocks and filling of the grid. Did this through the use of event listener. Object instances prototype started but still not in use.

--- JS/conn.js
+++ JS/conn.js
@@ -1,8 +1,110 @@
-const test = document.getElementById("tester");
+function gridCreator(){
+	arr = new Array(6);
+	for(let i = 0; i < arr.length; i++){
+		arr[i] = new Array(7) 
+	}
+	
+	for(let i = 0; i < 6; i++){
+		for(let j = 0; j < 7; j++){
+			arr[i][j] = 0;
+		}
+	}
+	return arr;
+}
 
-test.addEventListener("mouseover", change);
+function arrayPrinter2d(arr){
+	for(let i = 0; i < arr.length; i++){
+		console.log(arr[i]);
+	}
+}
 
-function change(){
-	test.style.backgroundColor = '#b83d3d';
+const grid = gridCreator();
+arrayPrinter2d(grid);
+
+
+/*Implement main board*/
+function Board(grid){
+	this.grid = grid;
+	this.red  = '#b83d3d';
+	this.blue = '#5275cc';
+	this.currentColor = true; //if true, red
+	
+	this.add = function(params) {
+		console.log('to implement');
+		
+	}
+	this.verifyWin = function(params) {
+		console.log('to implement');
+	}
+	this.cleanBoard = function(params) {
+		console.log('to implement');
+		
+	}
+	this.changeColor = function(params) {
+		this.currentColor = false;
+	}
 
 }
+
+
+/*Implement the sidebar object*/
+function Sidebar(){
+	this.score = 0;
+}
+/*Implement the timer object*/
+function Timer(){
+	this.time = 0;
+}
+
+
+
+
+/*Button Listeners Events*/
+const button0 = document.getElementById('choose0');
+const button1 = document.getElementById('choose1');
+const button2 = document.getElementById('choose2');
+const button3 = document.getElementById('choose3');
+const button4 = document.getElementById('choose4');
+const button5 = document.getElementById('choose5');
+const button6 = document.getElementById('choose6');
+
+
+button0.addEventListener('click', function(){
+	fill(0);
+})
+button1.addEventListener('click', function(){
+	fill(1);
+})
+button2.addEventListener('click', function(){
+	fill(2);
+})
+button3.addEventListener('click', function(){
+	fill(3);
+})
+button4.addEventListener('click', function(){
+	fill(4);
+})
+button5.addEventListener('click', function(){
+	fill(5);
+})
+button6.addEventListener('click', function(){
+	fill(6);
+})
+
+function fill(num){
+	let i = 0;
+	while(i < 5 && grid[i+1][num] === 0){
+		i++;
+	}
+	grid[i][num] = 1;
+	let test = document.getElementById( i + '' + num);
+	test.style.backgroundColor = '#b83d3d';
+}
+
+
+
+
+//const test = document.getElementById('5');
+//test.style.backgroundColor = 'yellow';
+
+
